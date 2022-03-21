58bfb0f1c9ba715b078620cc41a3d5f4a0eaf3f6
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-21 18:40:52 +0000

    create and use css modules

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,13 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
     render() {
         return (
-        <li>
+        <li className={styles.item}>
            <input 
            type="checkbox" 
+           className={styles.checkbox}
            checked={this.props.todo.completed} 
            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
            />  
