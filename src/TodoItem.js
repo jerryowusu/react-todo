ba94b820efbdbd20247ecb7931b50a5690c9212c
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-21 18:46:49 +0000

    apply styles as props  using ternary operator

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,6 +3,13 @@ import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
     render() {
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          }
+
         return (
         <li className={styles.item}>
            <input 
@@ -12,7 +19,9 @@ class TodoItem extends React.Component {
            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
            />  
            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)} >Delete</button>
+           <span style={this.props.todo.completed ? completedStyle : null}>
            {this.props.todo.title}
+           </span>
         </li>
             )
     }
