1ca54e9ebde41e016eda5d76ea74ca5af2ea5137
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-21 23:47:03 -0700

    arrange files into components

--- src/classBased/components/TodoContainer.js
+++ src/classBased/components/TodoContainer.js
@@ -10,11 +10,32 @@ class TodoContainer extends React.Component {
         todos: []
        };
 
-       componentDidMount() {
-           fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-           .then(response => response.json)
-           .then(data => this.setState({todos: data}));
-       }
+    //    componentDidMount() {
+    //        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    //        .then(response => response.json)
+    //        .then(data => this.setState({todos: data}));
+    //    }
+
+        componentDidUpdate(prevProps, prevState) {
+            if(prevState.todos !== this.state.todos) {
+                const temp = JSON.stringify(this.state.todos)
+                localStorage.setItem("todos", temp)
+            }
+        }  
+        
+        componentDidMount() {
+            const temp = localStorage.getItem("todos")
+            const loadedTodos = JSON.parse(temp)
+            if(loadedTodos) {
+                this.setState({
+                    todos: loadedTodos
+                })
+            }
+        }
+
+        componentWillUnmount() {
+            console.log("cleaning up.....")
+        }
 
        handleChange = (id) => {
           this.setState((prevState) =>  ({
